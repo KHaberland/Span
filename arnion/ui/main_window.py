12feffb06c544569a399596e9481892c11062c5c
KHaberland <kris.haberland@gmail.com>
2023-07-29 23:50:03 +0300

    формы списков данных

--- arnion/ui/main_window.py
+++ arnion/ui/main_window.py
@@ -2,7 +2,9 @@ import tkinter as tk
 
 from arnion.Data.departments_data import DepartmentDataHandler, DepartmentDataObject
 from arnion.Data.employees_data import EmployeeDataHandler, EmployeeDataObject
+from arnion.ui.departments_data_ui import DepartmentsWindow
 from arnion.ui.departments_reports_ui import DepartmentsReportWindow
+from arnion.ui.employees_data_ui import EmployeesWindow
 from arnion.ui.employees_reports_ui import EmployeesReportWindow
 
 
@@ -24,12 +26,12 @@ class MainWindow:
 
         # Добавление кнопки данных ОТДЕЛЫ
         btn_report = tk.Button(self.window, text="Отделы",
-                               font=('Helvetica', 10, 'bold'), bg='#ccffcc')
+                               font=('Helvetica', 10, 'bold'), bg='#ccffcc', command= self.do_list_departments)
         btn_report.place(x=25, y=100, width=120, height=50)
 
         # Добавление кнопки данных СОТРУДНИКИ
         btn_close = tk.Button(self.window, text="Сотрудники",
-                               font=('Helvetica', 10, 'bold'), bg='#ccffcc')
+                               font=('Helvetica', 10, 'bold'), bg='#ccffcc', command= self.do_list_employees)
         btn_close.place(x=160, y=100, width=120, height=50)
 
         # Добавление метки заголовка отчетов
@@ -65,6 +67,17 @@ class MainWindow:
         EmployeeDataHandler.insert(employee)
         print(employee.employee_id)
         print("Готово")
+
+    # открытие списка Отделы
+    def do_list_departments(self):
+        rpt = DepartmentsWindow()
+        rpt.open()
+
+    # открытие списка сотрудники
+    def do_list_employees(self):
+        rpt = EmployeesWindow()
+        rpt.open()
+
 
     # открытие отчета Отделы
 
